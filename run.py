736bc299108a60f4b8c685b45059637dc1a740b6
dyc1998108 <53322091+dyc1998108@users.noreply.github.com>
2020-06-26 16:11:20 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -5,13 +5,15 @@ import shutil
 import json
 
 # This run.py is just a simulation of the real situation, therefore inputs and outputs are abbreviated.
-# However, since this script only use Yichao Deng's permission to accessing flywheel,
-# projects may not be avaliable in some cases.
 
-user_id = 'stanfordlabs.flywheel.io:dddk2FvdSYhElu6BFK'
+# Using user api-key to get access to flywheel. One may change the api-key to get a higher privilege.
+user_api = 'dddk2FvdSYhElu6BFK'
+user_id = 'stanfordlabs.flywheel.io:' + user_api
 fw = flywheel.Client(user_id)
 self = fw.get_current_user()
+ori = os.getcwd()
 
+# This function is used for listing all of the files under a specific folder, recursively.
 def listall(root, path):
     if not os.path.isdir(os.path.join(path,root)):
         return [os.path.join(path,root)]
@@ -21,27 +23,40 @@ def listall(root, path):
         all.extend(listall(item, os.path.join(path,root)))
     return all
 
-acquisition = fw.lookup('wandell/Graphics test/image alignment/city3_14:17_v10.0_f74.04left_o270.00_201962618446/pos_5000_5000_5000')
+
+root = 'temporary'
 try:
-#     os.mkdir('temporary')
-#   This part of code shouldn't be run since we have already put these files under city3 in this repository, use them instead.
-#     i = 0
-#     for file in acquisition.files:
-#         print(file['name'])
-#         str = file['name']
-#         # The line below is necessary only if you are running on Windows.
-#         # However, I suggest not deleting it this time.
-#         new = str.replace(':', '_')
-#         acquisition.download_file(r'%s' % str, r'temporary/%s' % new)
-#         i += 1
-    os.chdir('temporary/')
-    fp = open('city3_14_17_v10.0_f74.04left_o270.00_201962618446_pos_5000_5000_5000_target.json', 'r', encoding='utf8')
+    # Making a temporary folder, which will be the main folder to work with, to put all of the files in a scene.
+    os.mkdir(root)
+    os.chdir(root)
+    root = os.path.abspath(os.getcwd())
+    # Using fw sdk to get city3 acquisition, and then downloading its files.
+    acquisition = fw.lookup('wandell/Graphics test/image alignment/city3_14:17_v10.0_f74.04left_o270.00_201962618446/pos_5000_5000_5000')
+    for file in acquisition.files:
+        print(file['name'])
+        str = file['name']
+        # Since windows don't support ':' in files' name, replace them with '_'.
+        # However, I suggest not deleting it this time.
+        new = str.replace(':', '_')
+        acquisition.download_file(r'%s' % str, r'%s' % new)
+
+    # Due to the limitation of Windows mentioned above, all of the ':' in file name originally are replaced by '_".
+    # In addition, code are modified correspond to then alternation of file name manually(not showing there).
+
+    # Using built-in function to handle json file, getting the target line.
+    pbrt_file = 'city3_14_17_v10.0_f74.04left_o270.00_201962618446_pos_5000_5000_5000_target.json'
+    fp = open(pbrt_file, 'r', encoding='utf8')
     js = json.load(fp)
+    # By split(), we can get all of the container(acquisition) id and target files' name.
     targets = js['fwAPI']['InfoList'].split(' ')
-    root = os.getcwd()
+
+    # Making new folder to save pbrt geometry and textures for future use.
     os.mkdir('scene/')
     os.mkdir('scene/PBRT/')
     os.mkdir('scene/PBRT/pbrt-geometry')
+    os.mkdir('textures/')
+
+    # Using for loop to iterate targets, then download every target files by fw sdk.
     for i in range(len(targets)):
         target = targets[i]
         print(target)
@@ -50,48 +65,67 @@ try:
             file_name = targets[i + 1]
             tmp.download_file(file_name, file_name)
             try:
+
+                # There are three situations:
+                # 1. target is data.zip
+                # 2. target is .exr file
+                # 3. other geometry zip files
+
+                # If target is data.zip.
                 if file_name == 'data.zip':
                     print('Is data.zip')
                     zip = zipfile.ZipFile(file_name)
                     zip.extractall(path=os.getcwd())
                     zip.close()
+
+                # If target is .exr file.
                 # I'm not sure whether this part is necessary, since I notice that in 5000.pbrt,
                 # .exr will only be used in root dir.
-                if file_name.endswith('.exr'):
+                elif file_name.endswith('.exr'):
                     shutil.copy(file_name, 'scene/PBRT/pbrt-geometry')
+
+                # If target is a geometry zip file.
                 else:
+                    # Building a folder to place all of the files in target zip file.
                     os.mkdir('%s' % file_name[:-4])
+                    # Unzipping target file.
                     zip = zipfile.ZipFile(file_name)
                     zip.extractall(path=file_name[:-4])
                     zip.close()
+
+                    # Copying all geometry pbrt to scene/PBRT/pbrt-geometry
                     os.chdir(file_name[:-4] + '/scene/PBRT/pbrt-geometry')
                     for i in os.listdir(os.getcwd()):
                         shutil.copy(i, root + '/scene/PBRT/pbrt-geometry')
                     os.chdir(root)
-                    os.mkdir('%s' % file_name[:-4])
-                    zip = zipfile.ZipFile(file_name)
-                    zip.extractall(path=file_name[:-4])
-                    zip.close()
-                    # As for textures, I just put them in a dir called textures under root.
+
+                    # Copying all textures to textures/
                     os.chdir(file_name[:-4] + '/textures')
                     for i in os.listdir(os.getcwd()):
                         shutil.copy(i, root + '/textures')
+                    os.chdir(root)
+
             finally:
-                os.chdir(root)
-                if file_name == 'data.zip': os.remove('data.zip')
+                # deleting all zip files downloaded and temporary folders used for place files.
+                if file_name == 'data.zip': os.remove(file_name)
                 elif os.path.exists('%s' % file_name[:-4]):
                     os.remove(file_name)
                     shutil.rmtree('%s' % file_name[:-4])
-    root = 'temporary'
+
+    # Building result/renderings for PBRT rendering.
     os.chdir(root)
     os.mkdir('result')
     os.mkdir('result/renderings')
-    output_file = 'result/renderings/city3_14_17_v10.0_f74.04left_o270.00_201962618446_pos_5000_5000_5000.dat'
-    curr_file = os.path.abspath('city3_14_17_v10.0_f74.04left_o270.00_201962618446_pos_5000_5000_5000.pbrt')
+    file = 'city3_14_17_v10.0_f74.04left_o270.00_201962618446_pos_5000_5000_5000'
+    output_file = 'result/renderings/%s.dat' % file
+    curr_file = os.path.abspath('%s.pbrt' % file)
     render_command = '/pbrt/pbrt-v3-spectral/build/pbrt --outfile %s %s' % (output_file, curr_file)
     os.system(render_command)
+
+# Checking if root exist, if true then deleting the folder.
 finally:
-    os.chdir('/flywheel/v0/')
-    if os.path.exists('temporary'):
+    os.chdir(ori)
+    if os.path.exists(root):
         print('If there is an exception raised, try again.')
-        # shutil.rmtree('temporary') # this line is used to reserve the resulting .dat file.
+        # deleting root for future use.
+        shutil.rmtree(root) 
