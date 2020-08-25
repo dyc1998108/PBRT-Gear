a73806e061f11f7ed551887993aad9f64b2e66c5
dyc1998108 <1454840107@qq.com>
2020-08-25 10:44:28 +0800

    adapt to simple scene and light

--- 0.2.0converter/run.py
+++ 0.2.0converter/run.py
@@ -62,92 +62,94 @@ try:
 
             # Changing this line if the situation is different.
             # It should always corresponding to the name of the file where target line located.
-            pbrt_file = '%s_target.json' % acquisition['label']
+            target_file = '%s_target.json' % acquisition['label']
 
-            # Using built-in function to handle json file, getting the target line.
-            fp = open(pbrt_file, 'r', encoding='utf8')
-            js = json.load(fp)
-            # By split(), we can get all of the container(acquisition) id and target files' name.
-            targets = js['fwAPI']['InfoList'].split(' ')
+            # Checking if the scene is a simple scene that have no target json file.
+            if os.path.exists(target_file):
+                # Using built-in function to handle json file, getting the target line.
+                fp = open(target_file, 'r', encoding='utf8')
+                js = json.load(fp)
+                # By split(), we can get all of the container(acquisition) id and target files' name.
+                targets = js['fwAPI']['InfoList'].split(' ')
 
-            # Making new folder to save pbrt geometry and textures for future use.
-            # Before making the directory, check if it exists.
-            if os.path.exists('scene/'):
-                print("Warning: Folder 'scene' already exists. Automatically deleting it now...")
-                shutil.rmtree('scene/')
-            os.mkdir('scene/')
-            os.mkdir('scene/PBRT/')
-            os.mkdir('scene/PBRT/pbrt-geometry')
+                # Making new folder to save pbrt geometry and textures for future use.
+                # Before making the directory, check if it exists.
+                if os.path.exists('scene/'):
+                    print("Warning: Folder 'scene' already exists. Automatically deleting it now...")
+                    shutil.rmtree('scene/')
+                os.mkdir('scene/')
+                os.mkdir('scene/PBRT/')
+                os.mkdir('scene/PBRT/pbrt-geometry')
 
-            if os.path.exists('textures/'):
-                print("Warning: Folder 'textures/' already exists. Automatically deleting it now...")
-                shutil.rmtree('textures/')
-            os.mkdir('textures/')
+                if os.path.exists('textures/'):
+                    print("Warning: Folder 'textures/' already exists. Automatically deleting it now...")
+                    shutil.rmtree('textures/')
+                os.mkdir('textures/')
 
-            # Using for loop to iterate targets, then download every target files by fw sdk.
-            for i in range(len(targets)):
-                target = targets[i]
-                print(target)  # For debugging.
+                # Using for loop to iterate targets, then download every target files by fw sdk.
+                for i in range(len(targets)):
+                    target = targets[i]
+                    print(target)  # For debugging.
 
-                # If a target is neither endswith zip nor endswith exr, then it should be a container id to look up.
-                if not target.endswith('.zip') and not target.endswith('.exr'):
+                    # If a target is neither endswith zip nor endswith exr, then it should be a container id to look up.
+                    if not target.endswith('.zip') and not target.endswith('.exr'):
 
-                    # 'tmp' is the container to be looked up.
-                    # 'file_name' is the target file to download, which can be get from target[i+1].
-                    tmp = fw.get(target)
-                    file_name = targets[i + 1]
-                    tmp.download_file(file_name, file_name)
+                        # 'tmp' is the container to be looked up.
+                        # 'file_name' is the target file to download, which can be get from target[i+1].
+                        tmp = fw.get(target)
+                        file_name = targets[i + 1]
+                        tmp.download_file(file_name, file_name)
 
-                    try:
+                        try:
 
-                        # There are three situations:
-                        # 1. target is data.zip
-                        # 2. target is .exr file
-                        # 3. other geometry zip files
+                            # There are three situations:
+                            # 1. target is data.zip
+                            # 2. target is .exr file
+                            # 3. other geometry zip files
 
-                        # If target is data.zip, we just need to unzip it to the main working directory.
-                        if file_name == 'data.zip':
-                            print('Is data.zip')
-                            zip = zipfile.ZipFile(file_name)
-                            zip.extractall(path=os.getcwd())
-                            zip.close()
+                            # If target is data.zip, we just need to unzip it to the main working directory.
+                            if file_name == 'data.zip':
+                                print('Is data.zip')
+                                zip = zipfile.ZipFile(file_name)
+                                zip.extractall(path=os.getcwd())
+                                zip.close()
 
-                        # If target is .exr file.
-                        # I'm not sure whether this part is necessary, since I notice that in 5000.pbrt,
-                        # .exr will only be used in root dir.
-                        elif file_name.endswith('.exr'):
-                            shutil.copy(file_name, 'scene/PBRT/pbrt-geometry')
+                            # If target is .exr file.
+                            # I'm not sure whether this part is necessary, since I notice that in 5000.pbrt,
+                            # .exr will only be used in root dir.
+                            elif file_name.endswith('.exr'):
+                                shutil.copy(file_name, 'scene/PBRT/pbrt-geometry')
 
-                        # If target is a geometry zip file, we need to unzip it and put its geometry and textures separately.
-                        else:
-                            # Building a folder to place all of the files in target zip file.
-                            os.mkdir('%s' % file_name[:-4])
-                            # Unzipping target file.
-                            zip = zipfile.ZipFile(file_name)
-                            zip.extractall(path=file_name[:-4])
-                            zip.close()
+                            # If target is a geometry zip file, we need to unzip it and put its geometry and textures separately.
+                            else:
+                                # Building a folder to place all of the files in target zip file.
+                                os.mkdir('%s' % file_name[:-4])
+                                # Unzipping target file.
+                                zip = zipfile.ZipFile(file_name)
+                                zip.extractall(path=file_name[:-4])
+                                zip.close()
 
-                            # Copying all geometry pbrt to scene/PBRT/pbrt-geometry
-                            os.chdir(file_name[:-4] + '/scene/PBRT/pbrt-geometry')
-                            for i in os.listdir(os.getcwd()):
-                                shutil.copy(i, root + '/scene/PBRT/pbrt-geometry')
-                            os.chdir(root)
-
-                            # Copying all textures to textures/. Some targets even don't  have textures.
-                            texture_dir = file_name[:-4] + '/textures'
-                            if os.path.exists(texture_dir):
-                                os.chdir(texture_dir)
+                                # Copying all geometry pbrt to scene/PBRT/pbrt-geometry
+                                os.chdir(file_name[:-4] + '/scene/PBRT/pbrt-geometry')
                                 for i in os.listdir(os.getcwd()):
-                                    shutil.copy(i, root + '/textures')
+                                    shutil.copy(i, root + '/scene/PBRT/pbrt-geometry')
                                 os.chdir(root)
 
-                    finally:
-                        # deleting all zip files downloaded and temporary folders used for place files.
-                        if file_name == 'data.zip':
-                            os.remove(file_name)
-                        elif os.path.exists('%s' % file_name[:-4]):
-                            os.remove(file_name)
-                            shutil.rmtree('%s' % file_name[:-4])
+                                # Copying all textures to textures/. Some targets even don't  have textures.
+                                texture_dir = file_name[:-4] + '/textures'
+                                if os.path.exists(texture_dir):
+                                    os.chdir(texture_dir)
+                                    for i in os.listdir(os.getcwd()):
+                                        shutil.copy(i, root + '/textures')
+                                    os.chdir(root)
+
+                        finally:
+                            # deleting all zip files downloaded and temporary folders used for place files.
+                            if file_name == 'data.zip':
+                                os.remove(file_name)
+                            elif os.path.exists('%s' % file_name[:-4]):
+                                os.remove(file_name)
+                                shutil.rmtree('%s' % file_name[:-4])
 
             # Building result/renderings for PBRT rendering.
             # Before making the directory, check if it exists.
@@ -164,7 +166,8 @@ try:
             pbrt = '%s' % acquisition['label']
             mesh = pbrt + '_mesh'
             depth = pbrt + '_depth'
-            files = [pbrt, mesh, depth]
+            light = pbrt + '_light'
+            files = [pbrt, mesh, depth, light]
             outputs = []
             for file in files:
                 if os.path.exists('%s.pbrt' % file):
